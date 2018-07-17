a62f349f0eb527c9a108a50c2fb7f8637f9d761d
Ammarf <35584084+Ammarf@users.noreply.github.com>
2018-07-17 16:32:14 +0900

    Rover: cruise mode edits

--- APMrover2/mode_manual.cpp
+++ APMrover2/mode_manual.cpp
@@ -9,15 +9,11 @@ void ModeManual::_exit()
 
 void ModeManual::update()
 {
-    float desired_steering, desired_throttle, desired_lateral;
+    // get pilot input
+    float desired_steering, desired_throttle;
     get_pilot_desired_steering_and_throttle(desired_steering, desired_throttle);
-    get_pilot_desired_lateral(desired_lateral);
 
-    // if vehicle is balance bot, calculate actual throttle required for balancing
-    if (rover.is_balancebot()) {
-        rover.balancebot_pitch_control(desired_throttle, rover.arming.is_armed());
-    }
-
+    // constrain steering and throttle inputs
     desired_throttle = constrain_float(desired_throttle, -100.0f, 100.0f);
     desired_steering = constrain_float(desired_steering, -4500.0f, 4500.0f);
 
@@ -25,31 +21,23 @@ void ModeManual::update()
     float scaled_throttle = desired_throttle / 100.0f;
     float scaled_steering = desired_steering / 4500.0f;
 
+    // calculate magnitude of input stick vector
     float magnitude_max = 1.0f;
-    float theta_max = 1.0f;
-
     float magnitude = safe_sqrt(sq(scaled_throttle) + sq(scaled_steering));
     if (fabsf(magnitude) > magnitude_max) {
         magnitude_max = magnitude;
     }
+    float throttle = magnitude / magnitude_max;
 
-    float speed = magnitude / magnitude_max;
-
+    // reverse steering when throttle is negative to correct heading angle
     if (is_negative(scaled_throttle)) {
         scaled_steering *= -1.0f;
     }
 
+    // calculate angle of input stick vector
     float theta = wrap_360_cd(atan2f(scaled_steering,scaled_throttle) * DEGX100);
-    //float desired_heading = ahrs.yaw_sensor - theta;
 
-    hal.console->printf("theta is %lf \n", theta);
-    hal.console->printf("yaw is %d \n", ahrs.yaw_sensor);
-
-
-    // copy RC scaled inputs to outputs
+    // run steering and throttle controllers
     calc_steering_to_heading(theta, _desired_speed < 0);
-    calc_throttle(speed, false, true);
-    //g2.motors.set_throttle(magnitude * 100.0f);
-    //g2.motors.set_steering(radians(desired_heading * 0.01f) * 4500.0f, false);
-
+    g2.motors.set_throttle(throttle);
 }
